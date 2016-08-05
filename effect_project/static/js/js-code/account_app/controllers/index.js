760120374ce5d74ea8e34aebaf6877a03105936e
fotis <fotios.floros@yahoo, com>
2016-08-05 23:04:50 +0300

    ..

--- effect_project/static/js/js-code/account_app/controllers/index.js
+++ effect_project/static/js/js-code/account_app/controllers/index.js
@@ -1,4 +1,5 @@
 // Define the `IndexController` controller on the `accountApp` module
 accountApp.controller('IndexController', function IndexController($scope) {
     $scope.test = 'test text';
+
 });
